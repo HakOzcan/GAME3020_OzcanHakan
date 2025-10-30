25ffc5241104252dfb427c43f2aaf4b0aad164af
Hakan Özcan <122551584+HakOzcan@users.noreply.github.com>
2025-10-30 00:56:48 -0400

    mover component added to enable safe house last chapter's garage door move upwards

---
+++ Source/GAME3020_OzcanHakan/Private/Components/Mover.cpp
@@ -0,0 +1,34 @@
+#include "Components/Mover.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UMover::BeginPlay()
+{
+    Super::BeginPlay();
+    OriginalLocation = GetOwner()->GetActorLocation();
+}
+
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    FVector TargetLocation = OriginalLocation;
+    if (ShouldMove)
+    {
+        TargetLocation = OriginalLocation + MoveOffset;
+    }
+    FVector CurrentLocation = GetOwner()->GetActorLocation();
+    float Speed = MoveOffset.Length() / MoveTime;
+
+    FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+    GetOwner()->SetActorLocation(NewLocation);
+}
+
+void UMover::SetShouldMove(bool NewShouldMove)
+{
+    ShouldMove = NewShouldMove;
+}
