b09254890a3a5f05e83a6635aa913cb252a15a30
Hakan Özcan <122551584+HakOzcan@users.noreply.github.com>
2025-10-29 21:34:20 -0400

    player controller for game added to assign active controller

--- Source/GAME3020_OzcanHakan/Private/Player Controller/PC_Game.cpp
+++ Source/GAME3020_OzcanHakan/Private/Player Controller/PC_Game.cpp
@@ -2,4 +2,22 @@
 
 
 #include "Player Controller/PC_Game.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
 
+void APC_Game::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void APC_Game::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+    // get the enhanced input subsystem
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+    {
+        // add the mapping context so we get controls
+        Subsystem->AddMappingContext(InputMappingContext, 0);
+    }
+}
--- Source/GAME3020_OzcanHakan/Public/Player Controller/PC_Game.h
+++ Source/GAME3020_OzcanHakan/Public/Player Controller/PC_Game.h
@@ -6,12 +6,22 @@
 #include "Player Controller/PC_Base.h"
 #include "PC_Game.generated.h"
 
+class UInputMappingContext;
+
 /**
  * 
  */
 UCLASS()
 class GAME3020_OZCANHAKAN_API APC_Game : public APC_Base
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+protected:
+
+    /** Input Mapping Context to be used for player input */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+    UInputMappingContext* InputMappingContext;
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void SetupInputComponent() override;
 };
